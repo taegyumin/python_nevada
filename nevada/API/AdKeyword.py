a6f6876248e01241215eb3fb2061b9509fbf648b
Taegyu Min <mtg821@gmail.com>
2020-02-17 02:00:15 +0900

    Update AdKeyword.py

--- nevada/API/AdKeyword.py
+++ nevada/API/AdKeyword.py
@@ -20,30 +20,6 @@ class UpdateAdKeywordObject:
         self.useGroupBidAmt = None
         self.userLock = None
 
-class ManagedKeywordInfoObject:
-    def __init__(self, json_def):
-        if type(json_def) is str:
-            json_def = json.loads(json_def)
-        s = json_def
-        self.PCPLMaxDepth = None if 'PCPLMaxDepth' not in s else s['PCPLMaxDepth']
-        self.editTm = None if 'editTm' not in s else s['editTm']
-        self.isAdult = None if 'isAdult' not in s else s['isAdult']
-        self.isBrand = None if 'isBrand' not in s else s['isBrand']
-        self.isLowSearchVolume = None if 'isLowSearchVolume' not in s else s['isLowSearchVolume']
-        self.isRestricted = None if 'isRestricted' not in s else s['isRestricted']
-        self.isSeason = None if 'isSeason' not in s else s['isSeason']
-        self.isSellProhibit = None if 'isSellProhibit' not in s else s['isSellProhibit']
-        self.keyword = None if 'keyword' not in s else s['keyword']
-        self.regTm = None if 'regTm' not in s else s['regTm']
-
-class ManagedKeywordObject:
-    def __init__(self, json_def):
-        if type(json_def) is str:
-            json_def = json.loads(json_def)
-        s = json_def
-        self.keyword = None if 'keyword' not in s else s['keyword']
-        self.managedKeyword = None if 'managedKeyword' not in s else ManagedKeywordInfoObject(s['managedKeyword'])
-
 class AdKeywordObject:
     def __init__(self, json_def):
         if type(json_def) is str:
@@ -71,7 +47,6 @@ class AdKeyword:
 
     AdKeywordList = List[AdKeywordObject]
     AdKeywordIdList = List[str]
-    KeywordsList = List[str]
 
     def get_adkeyword_list_by_label_id(self, nccLabelId: str) -> AdKeywordList:
         query = {'nccLabelId': nccLabelId}
@@ -146,14 +121,3 @@ class AdKeyword:
         ids = ",".join(ids)
         query = {'ids': ids}
         self.conn.delete('/ncc/keywords', query)
-
-    def managed_keyword_list(self, keywords: KeywordsList) -> ManagedKeywordObject:
-        keywords = ",".join(keywords)
-        query = {'keywords': keywords}
-        result = self.conn.get('/ncc/managedKeyword', query)
-
-        managedkeyword_list = []
-        for arr in result:
-            managedkeyword = ManagedKeywordObject(arr)
-            managedkeyword_list.append(managedkeyword)
-        return managedkeyword_list
