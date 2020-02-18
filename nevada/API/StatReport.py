bf2532b005493c3d5e50e1bc6f6a33f156f77824
Taegyu Min <mtg821@gmail.com>
2020-02-18 23:31:40 +0900

    Update StatReport.py

--- nevada/API/StatReport.py
+++ nevada/API/StatReport.py
@@ -22,35 +22,52 @@ class StatReportObject:
         self.status = None if 'status' not in s else s['status']
         self.updateTm = None if 'updateTm' not in s else s['updateTm']
 
-class StatReport:  # 대용량 보고서
+class StatReport:  #대용량 다운로드 보고서
     def __init__(self, base_url: str, api_key: str, secret_key: str, customer_id: int):
         self.conn = Connector(base_url, api_key, secret_key, customer_id)
 
     StatReportObjectList = List[StatReportObject]
 
-    def get_stat_report_list(self) -> StatReportObjectList:
+    def list(self, format=True) -> StatReportObjectList:
         result = self.conn.get('/stat-reports')
-        stat_list = []
-        for arr in result:
-            stat = StatReportObject(arr)
-            stat_list.append(stat)
-        return stat_list
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            stat_list = []
+            for arr in result:
+                stat = StatReportObject(arr)
+                stat_list.append(stat)
+            return stat_list
+        else:
+            print('Please Check the input value of format.')
 
-    def get_stat_report(self, reportJobId: str) -> StatReportObject:
+    def get(self, reportJobId: str, format=True) -> StatReportObject:
         result = self.conn.get('/stat-reports/' + reportJobId)
-        result = StatReportObject(result)
-        return result
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            return StatReportObject(result)
+        else:
+            print('Please Check the input value of format.')
 
-    def create_stat_report(self, CreateStatReportObject: CreateStatReportObject) -> StatReportObject:
-        data = jsonpickle.encode(CreateStatReportObject, unpicklable=False)
+    def create(self, reportTp, statDt, format=True) -> StatReportObject:
+        data = jsonpickle.encode(CreateStatReportObject(reportTp, statDt), unpicklable=False)
         data = json.loads(data)
         data = CommonFunctions.dropna(data)
         data_str = json.dumps(data)
-
         result = self.conn.post('/stat-reports', data_str)
-        result = StatReportObject(result)
-        return result
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            result = StatReportObject(result)
+            return result
+        else:
+            print('Please Check the input value of format.')
 
-    def delete_stat_reports(self, reportJobId: str):
+    def delete_all(self):
+        self.conn.delete('/stat-reports/')
+        return True
+
+    def delete_by_id(self, reportJobId: str):
         self.conn.delete('/stat-reports/' + reportJobId)
         return True
